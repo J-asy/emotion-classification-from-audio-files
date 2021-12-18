edf740bada9cb6cee3aafe442a6bb72e01ef6c74
J-asy <jsooyin@gmail.com>
2021-12-18 11:11:44 +0800

    Update live_predictions.py

--- live_predictions.py
+++ live_predictions.py
@@ -5,6 +5,7 @@ This file can be used to try a live prediction.
 import keras
 import librosa
 import numpy as np
+import sys
 
 from config import EXAMPLES_PATH
 from config import MODEL_DIR_PATH
@@ -23,16 +24,32 @@ class LivePredictions:
         self.path = MODEL_DIR_PATH + 'Emotion_Voice_Detection_Model.h5'
         self.loaded_model = keras.models.load_model(self.path)
 
+#     def make_predictions(self):
+#         """
+#         Method to process the files and create your features.
+#         """
+#         data, sampling_rate = librosa.load(self.file)
+#         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
+#         x = np.expand_dims(mfccs, axis=2)
+#         x = np.expand_dims(x, axis=0)
+#         predictions = self.loaded_model.predict_classes(x)
+#         print( "Prediction is", " ", self.convert_class_to_emotion(predictions))
+        
     def make_predictions(self):
         """
         Method to process the files and create your features.
         """
         data, sampling_rate = librosa.load(self.file)
         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
-        x = np.expand_dims(mfccs, axis=2)
-        x = np.expand_dims(x, axis=0)
-        predictions = self.loaded_model.predict_classes(x)
-        print( "Prediction is", " ", self.convert_class_to_emotion(predictions))
+        x = np.expand_dims(mfccs, axis=0)
+        x = np.expand_dims(x, axis=2)
+
+        # predict_x = self.loaded_model.predict_classes(x)
+        predict_x = self.loaded_model.predict(x)
+        classes_x=np.argmax(predict_x,axis=1)
+
+        print( "Prediction is", " ", self.convert_class_to_emotion(classes_x))
+
 
     @staticmethod
     def convert_class_to_emotion(pred):
@@ -56,8 +73,8 @@ class LivePredictions:
 
 
 if __name__ == '__main__':
-    live_prediction = LivePredictions(file=EXAMPLES_PATH + '03-01-01-01-01-02-05.wav')
+    audio_name = sys.argv[1]
+    live_prediction = LivePredictions(file=EXAMPLES_PATH + audio_name)
     live_prediction.loaded_model.summary()
     live_prediction.make_predictions()
-    live_prediction = LivePredictions(file=EXAMPLES_PATH + '10-16-07-29-82-30-63.wav')
-    live_prediction.make_predictions()
+
